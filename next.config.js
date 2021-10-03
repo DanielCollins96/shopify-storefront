305a0fd97abaa3482450b5992bdcb8edeb4e6d7a
Dan <danielmcollins1996@gmail.com>
2021-10-02 23:23:12 -0600

    Shopify client with shopify-buy library

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    env: {
+      SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+      SHOPIFY_STOREFRONT_TOKEN: process.env.SHOPIFY_STOREFRONT_TOKEN,
+      LOL: process.env.LOL
+    },
+    images: {
+      domains: ['cdn.shopify.com']
+    }
+  }
